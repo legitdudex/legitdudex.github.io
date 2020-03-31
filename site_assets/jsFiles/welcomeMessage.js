d05d1ab8a5c5c912bb38633453bb5a62549b3d92
Kang-hee Cho <kangchocho@gmail.com>
2020-03-30 22:53:31 -0400

    fixed margins

--- site_assets/jsFiles/welcomeMessage.js
+++ site_assets/jsFiles/welcomeMessage.js
@@ -73,7 +73,7 @@ function populateBoxes(text,size,fontSize){
     jQuery('<div/>', {
       id: 'row'+i
     }).appendTo('#intro');
-    $('#row'+i).css({"display":"flex","width":"100%","height":''+(100/map["size"]/2)+'%',"display":"inline-block","text-align":"center","justify-content":"center","align-items":"center","position":"relative"});
+    $('#row'+i).css({"margin":"0 auto","display":"flex","width":"100%","height":''+(100/map["size"]/2)+'%',"display":"inline-block","text-align":"center","justify-content":"center","align-items":"center","position":"relative"});
     for(var j = 0; j < map[i]["size"]; j++){
       jQuery('<span/>', {
         id: 'col'+i+''+j,
